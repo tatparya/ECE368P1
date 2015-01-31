adeb172085f15020260df4377837f1aa74e78a60
hthatukuru <hthatuku@purdue.edu>
2015-01-31 16:17:33 -0500

    modes

--- project1-A.c
+++ project1-A.c
@@ -1,9 +1,18 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-int main( int argc, const char* argv[] )
+int main( int argc, char ** argv )
 {
-	// New file!
-	// Commit Try #2!
+	int mode;
+
+	if ( argc < 1 )
+	{
+		mode = 1;
+	}
+	else if ( argc == 1 )
+	{
+		mode = 2;
+	}
+	
 	return 0;
 }
